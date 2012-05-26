72f9716410e21c39402726b7dd8daabdb2db262b
Seweryn Zeman <cadavre@cad-pc.(none)>
2012-05-26 09:49:10 +0200

    Initial Paxi commit

---
+++ gen/pro/jazzy/paxi/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package pro.jazzy.paxi;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/pro/jazzy/paxi/CarActivity.java
@@ -0,0 +1,14 @@
+package pro.jazzy.paxi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CarActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.cars);
+		
+		
+	}
+}
